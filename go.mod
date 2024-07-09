c61480a5c6ab91c25e5214181662f6ebc552192a
chenjianpei <chenjp9@chinatelecom.cn>
2024-07-09 14:41:06 +0800

    test submodule

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/no5stranger/submodule
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/no5stranger/submodule/sub"
+)
+
+func main() {
+	fmt.Println("hello")
+	sub.Echo()
+}
